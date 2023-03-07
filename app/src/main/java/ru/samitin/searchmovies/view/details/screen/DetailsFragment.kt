58aa1f81512098b594ac74e78a464e7526a6ea6a
evgenij <samitin.e@gmail.com>
2023-03-07 23:19:53 +1100

    2. Добавьте возможность оставлять заметку о просмотренном фильме, чтобы сохранять её в базе данных.

--- app/src/main/java/ru/samitin/searchmovies/view/details/screen/DetailsFragment.kt
+++ app/src/main/java/ru/samitin/searchmovies/view/details/screen/DetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
@@ -51,6 +52,12 @@ class DetailsFragment: Fragment() {
         val observer = Observer<AppState>{renderData(it)}
         viewModel.getLiveData().observe(viewLifecycleOwner,observer)
         viewModel.getDataFromServer(id)
+        binding.btnSendNote.setOnClickListener {
+            if (!binding.note.text.toString().equals(""))
+               Toast.makeText(context,binding.note.text.toString(),Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(context,"Введите текст",Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun renderData(appState: AppState) {
